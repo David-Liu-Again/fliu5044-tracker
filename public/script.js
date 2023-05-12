2ff5cc34cb44d3cca041d905baf994211b5344f7
Dave <fliu5044@uni.sydney.edu.au>
2023-05-12 12:54:25 +1000

    Added Local Storage for Tasks

--- public/script.js
+++ public/script.js
@@ -1,7 +1,10 @@
 // Setting up variables for our HTML elements using DOM selection
 const form = document.getElementById("taskform");
-const tasklist = document.getElementById("tasklist");
+const tasklistElem = document.getElementById("tasklist");
+// Create a new array called 'taskList'
+var taskList = [];
 
+// Set up submit button for Form 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
 
@@ -17,12 +20,33 @@ form.addEventListener("submit", function(event) {
   console.log(taskList)
 })
 
+// Create and display new task object, directly passing in the input parameters
+function addTask(name, type, rate, time, client) {
+  
+  let task = {
+    name,
+    type,
+    id: Date.now(),
+    date: new Date().toISOString(),
+    rate,
+    time,
+    client
+  }
+  // Add the object to the taskList array
+  taskList.push(task);
+  // Save to local storage
+  localStorage.setItem('savedTasks', JSON.stringify(taskList));
+  // Display task to user
+  displayTask(task);
+}
+
+// Add New Task object to DOM
 function displayTask(task) {
   let item = document.createElement("li");
   item.setAttribute("data-id", task.id);
   item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
 
-  tasklist.appendChild(item);
+  tasklistElem.appendChild(item);
 
   // Clear the value of the input once the task has been added to the page
   form.reset();
@@ -44,77 +68,31 @@ function displayTask(task) {
 
     // Make sure the deletion worked by logging out the whole array
     console.log(taskList)
-
+    localStorage.setItem('savedTasks', JSON.stringify(taskList));
     item.remove(); // Remove the task item from the page when button clicked
     // Because we used 'let' to define the item, this will always delete the right element
-
   })
 
   
 }
 
+//#region My Region
+// Fetch previous tasks from Local Storage
+let savedTasks = JSON.parse(localStorage.getItem('savedTasks'));
+  
+  // Check if the item doesn't exist in localStorage by seeing if it is null
+  if (savedTasks !== null){
 
-
-
-// Create an object called 'task'
-// Populate the properties based on the provided data model
-
-// Commented out now the object creation is included in the function
-
-// var task = {
-//   name: "Initial Sketches",
-//   type: "Concept Ideation",
-//   id: Date.now(),
-//   date: new Date().toISOString(),
-//   rate: 50,
-//   time: 5,
-//   client: "Google"
-// }
-
-// console.log(task);
-
-
-// Create an array called 'taskList'
-var taskList = [];
-
-// Create a function called 'addTask'
-// Give the function input parameters for: name, type, rate, time, client
-// Paste your object definition from above in the function
-// Replace the property values with the input paramaters
-// Add the object to the taskList array
-
-function addTask(name, type, rate, time, client) {
-
-  // Creating the object with the usual property:value syntax
-  // Create task object 
-  // let task = {
-  //   name: name,
-  //   type: type,
-  //   id: Date.now(),
-  //   date: new Date().toISOString(),
-  //   rate: rate,
-  //   time: time,
-  //   client: client
-  // }
-
-  // Creating the object, directly passing in the input parameters
-  let task = {
-    name,
-    type,
-    id: Date.now(),
-    date: new Date().toISOString(),
-    rate,
-    time,
-    client
+    // Loop through the countries and add their names as list items to the page
+    savedTasks.forEach((task) =>{
+      taskList.push(task);
+      displayTask(task);
+    });
   }
-
-  taskList.push(task);
-  displayTask(task);
-
-}
+//#region
 
 // Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+addTask(Math.floor(Math.random()*1000), "Concept Ideation", 50, 5, "Google");
 
 // Log the array to the console.
 console.log(taskList);
